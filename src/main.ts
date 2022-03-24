8b054a7561369180cdedd0ef6ee9bbde77ce931d
shoco55 <51412333+shoco55@users.noreply.github.com>
2022-03-24 10:53:21 +0900

    Sass導入

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
+import './assets/sass/common.scss';
+
 const app = createApp(App);
 app.use(ElementPlus);
 app.mount('#app');
