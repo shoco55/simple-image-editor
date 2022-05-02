6e1614cc1a307b06a3137ba918f482b1e44dd441
shoco55 <51412333+shoco55@users.noreply.github.com>
2022-05-02 17:39:31 +0900

    baseUrl, エイリアス設定の修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'simple-image-editor',
-  alias: [
-    {
-      find: '@/',
-      replacement: `./src/`,
-    },
-  ],
+  alias: {
+    '@/': `${__dirname}/src/`,
+  },
   plugins: [vue()],
   server: {
     host: true,
